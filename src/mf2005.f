38d87a160f20477ba00691cb2b146f7639388dfb
Bonelli <wbonelli@contractor.usgs.gov>
2026-02-07 07:59:33 -0500

    shaw nuff

--- src/mf2005.f
+++ src/mf2005.f
@@ -743,9 +743,9 @@ C     Write times to file if requested
         ENDIF
       ENDIF
 C
-C     Ensure output file is written (needed on macOS to flush buffers before exit)
-C     Flush only IOUT to avoid access violations with stream-access binary files on Intel compiler
-      CALL FLUSH()!(IOUT)
+C     Ensure output file is written on macOS/gfortran, but flush only IOUT to avoid
+C     access violations with stream-access binary files using ifx on linux/windows.
+      CALL FLUSH(IOUT)
 C
       RETURN
       END
